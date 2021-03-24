1d32f1db53a42b512f16e3ce67da421a922af286
Nehal Nagy <nehal.nagy@gmail.com>
2021-03-24 12:15:29 +0200

    Create API credentials on OpenWeatherMap.com

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-
+let baseURL = 'api.openweathermap.org/data/2.5/weather?q=';
+let apikey = '6a50dde02db3c5ae2fc05a22e77ffc2d';
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
